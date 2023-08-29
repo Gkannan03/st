2ad5fe75449eb8f6098b1682f485da8c7478e67a
Gkannan03 <46295857+Gkannan03@users.noreply.github.com>
2023-08-29 14:25:59 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.write('Hello, Welome')
 from PIL import Image
-image_1 = str(current_dir / "image_file" / "Logo-removebg.png")
+from pathlib import Path
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+image_1 = str(current_dir / "Logo-removebg.png")
 pil_image = Image.open(image_1)
 image_array = np.array(pil_image)
 st.image(image_array)
