d0e9f59cc5853a2ad93e733eb120847e4779298d
Gkannan03 <46295857+Gkannan03@users.noreply.github.com>
2023-08-29 14:24:33 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.write('Hello, Welome')
+from PIL import Image
+image_1 = str(current_dir / "image_file" / "Logo-removebg.png")
+pil_image = Image.open(image_1)
+image_array = np.array(pil_image)
+st.image(image_array)
